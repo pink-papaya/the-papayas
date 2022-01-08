ec7cdf9b10efee8cf6969b565d27aeb8c489b2fb
Ricardo Tavares <rnunot@gmail.com>
2022-01-08 16:18:50 +0000

    init

---
+++ src/components/FileTree.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div
+      v-for="item in items"
+      :key="item.name"
+      :class="item.type"
+      class="item"
+      @click.self="toggle(item)"
+    >
+      <span v-if="item.type === 'folder'">📁</span>
+      <span v-if="item.type === 'song'">♫</span>
+
+      {{ item.name }}
+
+      <div v-if="item.type === 'folder' && item.children" class="children">
+         <FileTree v-show="itemStateMap[item.name]" :items="item.children" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang=ts>
+  import { ref } from 'vue';
+
+  interface Song {
+    name: string;
+    type: 'song';
+    updateDate: string;
+  }
+
+  interface Folder {
+    name: string;
+    type: 'folder';
+    children: (Folder|Song)[];
+  }
+
+  defineProps<{ items: (Folder|Song)[] }>()
+
+  const itemStateMap = ref<Record<string, boolean>>({});
+
+  function toggle(item: Folder | Song) {
+    if (item.type === 'folder') {
+      itemStateMap.value[item.name] = !itemStateMap.value[item.name];
+    }
+  }
+</script>
+
+<style lang="css">
+.item  {
+  padding: 8px;
+  margin: 8px;
+
+  text-align: start;
+
+  border: 1px solid #ccc;
+}
+
+.folder {
+  background: lightcoral;
+  cursor: pointer;
+}
+
+.song {
+  cursor: default;
+  background: lightpink;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,8 @@
+<template>
+  <FileTree :items="songs" />
+</template>
+
+<script setup lang="ts">
+  import FileTree from './FileTree.vue';
+  import songs from '../assets/songs.json';
+</script>

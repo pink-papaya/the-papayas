c39ad1f1513374c52bc0cb0a3f20d5bf37ab69df
Ricardo Tavares <rnunot@gmail.com>
2022-01-10 15:25:28 +0000

    feat: Add debounce to search

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,6 +13,7 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue';
 import cloneDeep from 'lodash/cloneDeep';
+import debounce from 'lodash/debounce';
 import FileTree from './FileTree.vue';
 import songs from '../assets/songs.json';
 import { Folder, Collection } from '../types';
@@ -21,26 +22,28 @@ const query = ref('');
 
 const filteredItems = ref(cloneDeep(songs as Collection));
 
-watch(query, () => {
-  function filterFunction(items: Collection, parent?: Folder) {
-    items.forEach((item) => {
-      if (item.type === 'folder') {
-        filterFunction(item.children, item);
-        return;
-      }
-
-      // eslint-disable-next-line no-param-reassign
-      item.isVisible = item.name
-        .toLowerCase()
-        .includes(query.value.toLowerCase());
-    });
-
-    if (parent) {
-      // eslint-disable-next-line no-param-reassign
-      parent.isVisible = items.some((item) => item.isVisible !== false);
+function filterFunction(items: Collection, parent?: Folder) {
+  items.forEach((item) => {
+    if (item.type === 'folder') {
+      filterFunction(item.children, item);
+      return;
     }
-  }
 
-  filterFunction(filteredItems.value);
+    // eslint-disable-next-line no-param-reassign
+    item.isVisible = item.name
+      .toLowerCase()
+      .includes(query.value.toLowerCase());
+  });
+
+  if (parent) {
+    // eslint-disable-next-line no-param-reassign
+    parent.isVisible = items.some((item) => item.isVisible !== false);
+  }
+}
+
+const debouncedFilter = debounce(filterFunction, 200);
+
+watch(query, () => {
+  debouncedFilter(filteredItems.value);
 });
 </script>

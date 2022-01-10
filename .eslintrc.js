f52168b9c054c42a6443cc8bd2c1cf3c73c1eafb
Ricardo Tavares <rnunot@gmail.com>
2022-01-10 15:08:24 +0000

    style: Adjust linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,16 @@
 module.exports = {
   env: {
     node: true,
+    'vue/setup-compiler-macros': true,
   },
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: ['@typescript-eslint'],
   extends: [
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     '@vue/airbnb',
     '@vue/typescript/recommended',
     'plugin:prettier/recommended',

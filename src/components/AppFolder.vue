3956fc94d215d8daf42ab2e8751d45d434f09e92
Ricardo Tavares <rnunot@gmail.com>
2023-02-27 18:09:28 +0000

    feat: Move song and folder logic into separate components, improve mobile style

---
+++ src/components/AppFolder.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="cursor-pointer font-bold">
+    <div
+      :class="{ 'border-transparent': depth === 0 }"
+      class="sticky top-[81px] z-10 flex items-center gap-x-1 bg-slate-800 fill-pink-200 p-1 capitalize text-pink-200 hover:fill-pink-300 hover:text-pink-300 sm:top-[120px]"
+    >
+      <mdi-icon v-show="!isOpen" :path="mdiFolder" :size="18" class="inline" />
+      <mdi-icon
+        v-show="isOpen"
+        :path="mdiFolderOpen"
+        :size="18"
+        class="inline"
+      />
+
+      {{ folder.name }}
+    </div>
+    <div v-if="folder.children" class="children">
+      <FileTree v-show="isOpen" :items="folder.children" :depth="depth + 1" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { mdiFolder, mdiFolderOpen } from '@mdi/js';
+import { Folder } from '../types';
+import FileTree from './FileTree.vue';
+import MdiIcon from './MdiIcon.vue';
+
+defineProps<{ folder: Folder; depth: number; isOpen: boolean }>();
+</script>
+
+<style scoped></style>
--- src/components/FileTree.vue
+++ src/components/FileTree.vue
@@ -4,57 +4,18 @@
       <div
         v-if="item.isVisible !== false"
         :class="{ [item.type]: true, first: depth === 0 }"
-        class="item bg-slate-800"
+        class="item border-l-2 border-pink-200 bg-slate-800"
         :tabindex="item.type === 'folder' ? 0 : -1"
         @keyup.enter.stop="toggle(item)"
         @click.stop="toggle(item)"
       >
-        <div class="item-name bg-slate-800">
-          <template v-if="item.type === 'folder'">
-            <mdi-icon
-              v-show="itemStateMap[item.name] === false"
-              :path="mdiFolder"
-              :size="18"
-              class="inline"
-            />
-            <mdi-icon
-              v-show="itemStateMap[item.name] !== false"
-              :path="mdiFolderOpen"
-              :size="18"
-              class="inline"
-            />
-          </template>
-
-          <mdi-icon
-            v-if="item.type === 'song'"
-            :path="mdiMusicNote"
-            :size="18"
-            class="inline"
-          />
-
-          {{ item.name }}
-        </div>
-        <div v-if="item.type === 'song'" style="height: 18px">
-          <a
-            :href="
-              'https://www.youtube.com/results?search_query=' +
-              encodeURIComponent(item.name)
-            "
-            target="_blank"
-            rel="noopener noreferrer"
-            class="fill-slate-400 hover:fill-red-700"
-            title="Search song name on youtube"
-          >
-            <mdi-icon :path="mdiYoutube" :size="18" />
-          </a>
-        </div>
-        <div v-if="item.type === 'folder' && item.children" class="children">
-          <FileTree
-            v-show="itemStateMap[item.name] !== false"
-            :items="item.children"
-            :depth="depth + 1"
-          />
-        </div>
+        <AppFolder
+          v-if="item.type === 'folder'"
+          :folder="item"
+          :is-open="itemStateMap[item.name] !== false"
+          :depth="depth"
+        />
+        <AppSong v-if="item.type === 'song'" :song="item" />
       </div>
     </template>
   </div>
@@ -62,9 +23,9 @@
 
 <script setup lang="ts">
 import { onBeforeMount, ref } from 'vue';
-import { mdiYoutube, mdiFolder, mdiFolderOpen, mdiMusicNote } from '@mdi/js';
 import { Collection, Folder, Song } from '../types';
-import MdiIcon from './MdiIcon.vue';
+import AppFolder from './AppFolder.vue';
+import AppSong from './AppSong.vue';
 
 const props = withDefaults(
   defineProps<{ items: Collection; depth?: number }>(),
@@ -97,11 +58,18 @@ function toggle(item: Folder | Song) {
 
 <style>
 .item {
-  @apply border-l-2 border-pink-200 fill-slate-300 font-medium text-slate-300;
-
   &.first {
+    &.folder {
+      @apply border-transparent;
+    }
+
+    @apply mx-0;
+
+    @screen sm {
+      margin-right: 16px;
+      margin-left: 16px;
+    }
     padding-right: 16px;
-    margin-right: 16px;
   }
 
   padding: 10px 0 6px 8px;
@@ -110,40 +78,6 @@ function toggle(item: Folder | Song) {
   &:last-child:not(.first) {
     padding-bottom: 8px;
     @apply border-b-2 border-b-gray-900;
-  }
-
-  text-align: start;
-}
-
-.folder {
-  @apply cursor-pointer border-l-2 border-slate-400 border-l-pink-200 font-bold;
-
-  &.first {
-    @apply border-transparent;
-  }
-
-  > .item-name {
-    @apply sticky z-10 fill-pink-200 p-1 capitalize text-pink-200;
-
-    &:hover {
-      @apply fill-pink-300 text-pink-300;
-    }
-
-    top: 81px;
-    @screen md {
-      top: 120px;
-    }
-  }
-}
-
-.song {
-  cursor: default;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  > .item-name {
-    @apply pl-1;
   }
 }
 </style>
